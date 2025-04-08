584c230cb22700833b79f81cf189b0d2ecf5d399
sagar-rathod-devops <sagar.rathod.devops@gmail.com>
2025-04-08 17:18:21 +0530

    Aws Lambda Function Update

--- main.go
+++ main.go
@@ -1,8 +1,42 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+type Response struct {
+	Message string `json:"message"`
+}
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	fmt.Printf("Received request: %+v\n", request)
+
+	resp := Response{
+		Message: "Hello from AWS Lambda using Golang!",
+	}
+
+	body, err := json.Marshal(resp)
+	if err != nil {
+		return events.APIGatewayProxyResponse{
+			StatusCode: 500,
+			Body:       `{"error": "Internal Server Error"}`,
+		}, nil
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: 200,
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+		Body: string(body),
+	}, nil
+}
 
 func main() {
-	// This is a simple Go program that prints "Hello, World!" to the console.
-	fmt.Println("Hello, World!")
+	lambda.Start(handler)
 }
